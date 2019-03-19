7ffa9d17887a392b35f6ea06ea605162420a6815
Mark Ratjens <mark@ratjens.com>
2019-03-19 12:44:01 +1100

    Initial Commit

---
+++ Duwabili/Duwabili.h
@@ -0,0 +1,19 @@
+//
+//  Duwabili.h
+//  Duwabili
+//
+//  Created by Mark Ratjens on 19/3/19.
+//  Copyright © 2019 Nyasa Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Duwabili.
+FOUNDATION_EXPORT double DuwabiliVersionNumber;
+
+//! Project version string for Duwabili.
+FOUNDATION_EXPORT const unsigned char DuwabiliVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Duwabili/PublicHeader.h>
+
+
